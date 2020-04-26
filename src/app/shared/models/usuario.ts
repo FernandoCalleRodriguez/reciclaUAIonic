bd056a121b821c79b54015a314b74d29d5d68f25
ferky_27 <ferky_27@hotmail.com>
2020-04-26 10:52:55 +0200

    Añadido usuario model

---
+++ src/app/shared/models/usuario.ts
@@ -0,0 +1,11 @@
+export class Usuario {
+    Id?: number;
+    Nombre?: string;
+    Apellidos?: string;
+    Pass?: string;
+    Email?: string;
+    Fecha?: Date;
+    EmailVerificado?: string;
+    Borrado?: boolean;
+    Puntuacion?: boolean;
+}
