e0fe83a446c1b49901b3234bc0998d8d018266e3
angelasbrizzi <62255433+angelasbrizzi@users.noreply.github.com>
2020-04-26 22:54:42 +0200

    Page NotaInfo

---
+++ src/app/notainfo/notainfo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notainfo',
+  templateUrl: './notainfo.page.html',
+  styleUrls: ['./notainfo.page.scss'],
+})
+export class NotainfoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
