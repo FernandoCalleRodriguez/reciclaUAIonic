d0ca79b8122f83c7b95d92bd7124417ff8760509
ferky_27 <ferky_27@hotmail.com>
2020-04-26 13:12:30 +0200

    Añadido servicio configuracio

---
+++ src/app/shared/services/configuracion.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {Usuario} from '../models/usuario';
+import {UsuarioService} from './usuario.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ConfiguracionService {
+
+    constructor(private usuarioService: UsuarioService) {
+    }
+
+    esEmailRepetido(email: string, usuario: Usuario): Promise<{ [s: string]: boolean }> {
+
+        return new Promise<{ [p: string]: boolean }>(resolve => {
+            this.usuarioService.obtenerUsuarioPorEmail(email).subscribe(result => {
+                if ((result && !usuario) || (result && usuario && result.Id != usuario.Id)) {
+                    console.log(' Emmail no valido');
+                    return resolve({emailEsNoValido: true});
+                } else {
+                    console.log(' Emmail valido');
+                    return resolve(null);
+
+                }
+
+            }, error => {
+                return resolve(null);
+            });
+
+        });
+    }
+}
