b713f5263dbc91bceeb2bf513f79e086323bd9e0
angelasbrizzi <62255433+angelasbrizzi@users.noreply.github.com>
2020-05-13 15:58:57 +0200

    Lectura notas info

--- src/app/notainfo/notainfo.page.ts
+++ src/app/notainfo/notainfo.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Nota } from '../shared/models/nota';
+import {NotaService} from '../shared/services/nota.service';
+import {Router} from '@angular/router';
+import {ToastController} from '@ionic/angular';
+import {AutenticacionService} from '../shared/services/autenticacion.service';
 
 @Component({
   selector: 'app-notainfo',
@@ -6,8 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notainfo.page.scss'],
 })
 export class NotainfoPage implements OnInit {
+  Notas: Nota[];
+  Nota: Nota;
 
-  constructor() { }
+  constructor(private  notaService: NotaService,
+              private  router: Router,
+              private toastController: ToastController,
+              private  autenticationService: AutenticacionService) {
+    // this.autenticationService.estaAutenticado();
+    this.notaService.obtenerTodasNotas().subscribe(res => {
+      this.Notas = res;
+    });
+  }
 
   ngOnInit() {
   }
