d6aebbb4068b7781dbd64a121a32a3f3748a85ba
Agulló <joseantonioagullo@gmail.com>
2020-05-15 14:22:27 +0200

    Meojoras en los detalles de duda. Añadidos botones edit/borrar

--- src/app/foro/detalle-duda/detalle-duda.page.ts
+++ src/app/foro/detalle-duda/detalle-duda.page.ts
@@ -4,6 +4,9 @@ import {DudaService} from '../../shared/services/duda.service';
 import {RespuestaService} from '../../shared/services/respuesta.service';
 import {Duda} from '../../shared/models/duda';
 import {Respuesta} from '../../shared/models/respuesta';
+import {AutenticacionService} from '../../shared/services/autenticacion.service';
+import {UsuarioService} from '../../shared/services/usuario.service';
+import {Usuario} from '../../shared/models/usuario';
 
 @Component({
     selector: 'app-detalle-duda',
@@ -14,9 +17,10 @@ export class DetalleDudaPage implements OnInit {
 
     public duda: Duda = null;
     public respuestas: Respuesta[] = null;
+    public usuario: Usuario = null;
 
     constructor(protected route: ActivatedRoute, protected dudaService: DudaService,
-                protected respuestaService: RespuestaService) {
+                protected respuestaService: RespuestaService, protected  usuarioService: UsuarioService) {
         const id: number = parseInt(this.route.snapshot.paramMap.get('id'), 10);
         this.dudaService.getDudaById(id).subscribe(d => {
           this.duda = d;
@@ -27,6 +31,8 @@ export class DetalleDudaPage implements OnInit {
     }
 
     ngOnInit() {
+        this.usuarioService.getLoggedUser().subscribe(u => {
+            this.usuario = u;
+        });
     }
-
 }
